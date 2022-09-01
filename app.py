7ce3a7ba58f8b9f979951a4eaba5d5b3bc4ce6c1
PavithiraRajen <108916815+PavithiraRajen@users.noreply.github.com>
2022-09-01 17:48:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 import yake
 app = Flask(__name__)
 @app.route('/tag',methods=['GET','POST'])
-def endpoint2():
+def tag():
   req = request.get_json()
   list1 = []
   list1.append({'html':req['article-content']})
@@ -35,49 +35,6 @@ def endpoint2():
   #y = json.dumps(d1)
   return d1
 
-# def preprocess(c):
-#     clean = re.compile('<.*?>')
-#     return re.sub(clean, '', c)
-    
-
-    
-# def yake_kw(i):
-#     i = preprocess(i)
-#     kw_extractor = yake.KeywordExtractor()
-#     language = "en"
-#     max_ngram_size = 3
-#     deduplication_threshold = 0.1
-#     numOfKeywords = 3
-#     custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None, stopwords=None) 
-#     keywords = custom_kw_extractor.extract_keywords(i)
-#     yake_key = []
-#     for kw, v in keywords:
-#       #print("Keyphrase: ",kw, ": score", v)
-#       yake_key.append(kw.title())
-#     return yake_key
-# ################# 
-
-# @app.route('/tag-api-2', methods=['GET','POST'])
-# def index():
-#   req = request.get_json()
-#   list1 = []
-#   list1.append({'html':req['article-content']})
-#   i = ', '.join(strings['html'] for strings in list1)
-#   print(i)
-#   print(type(i))
-#   print("Preprocess works")
-#   kw_model = KeyBERT(model="all-MiniLM-L6-v2")
-#   clean = re.compile('<.*?>')
-#   re.sub(clean, '', i)
-#   language = "english"
-#   max_ngram_size = (1, 3)
-#   numOfKeywords = 3
-#   keywords = kw_model.extract_keywords(i,keyphrase_ngram_range= max_ngram_size,stop_words= language,top_n= numOfKeywords, diversity=0.2)
-#   keywords_list= list(dict(keywords).keys())
-#   final_keywords_list = [i.title() for i in keywords_list]
-#   d1 = dict(enumerate(final_keywords_list))
-#   y = json.dumps(d1)
-#   return y
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
