9b0a4cc350dfc5650f1ce1e04892984a2f2ca872
PavithiraRajen <108916815+PavithiraRajen@users.noreply.github.com>
2022-09-01 13:09:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def endpoint2():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000,debug=True)
+    app.run()
